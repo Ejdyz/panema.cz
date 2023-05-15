6275a303b74f4e25c3fe4a20d2b3f9bef56105bf
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-15 22:01:03 +0200

    fix: chronology of links Navbar

--- panema/src/Components/NavBar.js
+++ panema/src/Components/NavBar.js
@@ -25,10 +25,10 @@ export default function NavBar(props){
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Link to="/" style={{ textDecoration: 'none' }}><Nav.Link as={"span"}  style={props.currentPage === "main" ? {borderBottom: "2px solid rgb(255 197 88)"}:{}} active={props.currentPage === "main"} >Úvod</Nav.Link></Link>
-            <Link to="/kontakt" style={{ textDecoration: 'none' }}><Nav.Link as={"span"} style={props.currentPage === "contact" ? {borderBottom: "2px solid rgb(255 197 88)"}:{}} active={props.currentPage === "contact"} >Kontakt</Nav.Link></Link>
             <Link to="/technologie" style={{ textDecoration: 'none' }}><Nav.Link as={"span"} style={props.currentPage === "technology" ? {borderBottom: "2px solid rgb(255 197 88)"}:{}} active={props.currentPage === "technology"} >Technologie</Nav.Link></Link>
-            <Link to="/reference" style={{ textDecoration: 'none' }}><Nav.Link as={"span"} style={props.currentPage === "reference" ? {borderBottom: "2px solid rgb(255 197 88)"}:{}} active={props.currentPage === "reference"} >Referece</Nav.Link></Link>
             <Link to="/fotografie" style={{ textDecoration: 'none' }}><Nav.Link as={"span"} style={props.currentPage === "fotography" ? {borderBottom: "2px solid rgb(255 197 88)"}:{}} active={props.currentPage === "fotography"}>Fotografie</Nav.Link></Link>
+            <Link to="/reference" style={{ textDecoration: 'none' }}><Nav.Link as={"span"} style={props.currentPage === "reference" ? {borderBottom: "2px solid rgb(255 197 88)"}:{}} active={props.currentPage === "reference"} >Referece</Nav.Link></Link>
+            <Link to="/kontakt" style={{ textDecoration: 'none' }}><Nav.Link as={"span"} style={props.currentPage === "contact" ? {borderBottom: "2px solid rgb(255 197 88)"}:{}} active={props.currentPage === "contact"} >Kontakt</Nav.Link></Link>
           </Nav>
           <Nav>
           <Link to="/kestazeni" style={{ textDecoration: 'none' }}><Nav.Link as={"span"} style={props.currentPage === "download" ? {borderBottom: "2px solid rgb(255 197 88)"}:{}} active={props.currentPage === "download"}>Ke Stažení</Nav.Link></Link>

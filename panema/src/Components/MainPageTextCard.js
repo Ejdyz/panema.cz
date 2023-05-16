3568f83809c37a3506d41fe9ab9df4c386877ef6
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-16 12:08:55 +0200

    feat: converted main page text code to component

---
+++ panema/src/Components/MainPageTextCard.js
@@ -0,0 +1,13 @@
+export function MainPageTextCard() {
+  return (
+    <div style={{ backgroundColor: "#e0a535", display: "grid", placeItems: "center", }}>
+      <div style={{ backgroundColor: "#191c1e", height: "auto", width: "80%", borderRadius: "70px 20px ", textAlign: "center", padding: "30px", margin: "30px", color: 'white' }}>
+        <h4>Společnost <h4 style={{ color: "#e0a535", display: "inline" }}>PANEMA </h4> s.r.o. byla založena v roce 2004 a zabývá se bezvýkopou výstavbou a rekonstrukcí inženýrských sítí.</h4>
+        <br />
+        <h4><h4 style={{ color: "#e0a535", display: "inline" }}>Rádi</h4> vám poradíme jakou technologii při stavebních pracech zvolit. </h4>
+        <br />
+        <u><h4>Naším cílem je  <u style={{ color: '#e0a535' }}><h4 style={{ color: "#e0a535", display: "inline" }}>spokojený</h4> </u>zákazník!</h4></u>
+      </div>
+    </div>
+  )
+} 

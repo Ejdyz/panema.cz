cdb5a6f4420e9cdbe7e41875552450073c764d6a
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-16 16:02:51 +0200

    style: bigger text on main page

--- panema/src/Pages/MainPage.js
+++ panema/src/Pages/MainPage.js
@@ -11,8 +11,8 @@ export function MainPage() {
 
             <div style={{ position: "relative" }}>
               <img src="/images/homePage.png" alt="Image" style={{ height: "auto", display: "block", width: "100%", borderRadius: "20px 20px 0px 0px", opacity: .6 }} />
-              <span style={{ position: "absolute", top: "35%", left: "50%", transform: " translate(-50%, -50%)", color: "white" }}>
-                <h1 style={{ textAlign: "center", color: "white" }}>Úvod</h1>
+              <span style={{ position: "absolute", top: "40%", left: "50%", transform: " translate(-50%, -50%)", color: "white" }}>
+                <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Úvod</h1>
                 <hr style={{ width: "200px", color: "#e7b242", opacity: 1 }}></hr>
               </span>
             </div>

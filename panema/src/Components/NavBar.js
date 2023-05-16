b9ffb575473ae03c25fa143e11f7e9075fe9e8de
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-16 11:43:57 +0200

    feat: added icons to Navbar

--- panema/src/Components/NavBar.js
+++ panema/src/Components/NavBar.js
@@ -4,142 +4,146 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
 import { Hidden } from "react-grid-system";
+import { Person, House, Gear, Image, Download, Briefcase } from "react-bootstrap-icons";
 
 export default function NavBar(props) {
   return (
     <>
-      <ThemeProvider
-        breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
-        minBreakpoint="xxs"
-      >
-        <Hidden md sm xs xxs>
-          <div
+      <Hidden md sm xs xxs>
+        <div
+          style={{
+            backgroundColor: "#212529",
+            display: "flex",
+            width: "auto",
+            justifyContent: "center",
+            padding: "5px",
+          }}
+        >
+          <img
+            src="\images\logo.png"
+            alt="Logo"
             style={{
-              backgroundColor: "#212529",
-              display: "flex",
-              width: "auto",
-              justifyContent: "center",
-              padding: "5px",
+              backgroundColor: "#7c7c7c",
+              padding: "2px",
+              maxHeight: "70px",
+              width: "",
             }}
-          >
-            <img
-              src="\images\logo.png"
-              alt="Logo"
-              style={{
-                backgroundColor: "#7c7c7c",
-                padding: "2px",
-                maxHeight: "70px",
-                width: "",
-              }}
-            />
-          </div>
-        </Hidden>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
-            <Link to="/" style={{ textDecoration: "none" }}>
-              <Navbar.Brand>
-                <Hidden lg xl xxl xxxl>
-                  <img
-                    src="\images\logo.png"
-                    alt="Logo"
-                    style={{
-                      backgroundColor: "#7c7c7c",
-                      padding: "2px",
-                      maxHeight: "32px",
-                      display: { md: "inherit", xs: "none" },
-                    }}
-                  />
-                </Hidden>
-              </Navbar.Brand>
-            </Link>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Link to="/" style={{ textDecoration: "none" }}>
-                  <Nav.Link
-                    as={"span"}
-                    style={
-                      props.currentPage === "main"
-                        ? { borderBottom: "2px solid rgb(255 197 88)" }
-                        : {}
-                    }
-                    active={props.currentPage === "main"}
-                  >
-                    Úvod
-                  </Nav.Link>
-                </Link>
-                <Link to="/technologie" style={{ textDecoration: "none" }}>
-                  <Nav.Link
-                    as={"span"}
-                    style={
-                      props.currentPage === "technology"
-                        ? { borderBottom: "2px solid rgb(255 197 88)" }
-                        : {}
-                    }
-                    active={props.currentPage === "technology"}
-                  >
-                    Technologie
-                  </Nav.Link>
-                </Link>
-                <Link to="/fotografie" style={{ textDecoration: "none" }}>
-                  <Nav.Link
-                    as={"span"}
-                    style={
-                      props.currentPage === "fotography"
-                        ? { borderBottom: "2px solid rgb(255 197 88)" }
-                        : {}
-                    }
-                    active={props.currentPage === "fotography"}
-                  >
-                    Fotografie
-                  </Nav.Link>
-                </Link>
-                <Link to="/reference" style={{ textDecoration: "none" }}>
-                  <Nav.Link
-                    as={"span"}
-                    style={
-                      props.currentPage === "reference"
-                        ? { borderBottom: "2px solid rgb(255 197 88)" }
-                        : {}
-                    }
-                    active={props.currentPage === "reference"}
-                  >
-                    Referece
-                  </Nav.Link>
-                </Link>
-                <Link to="/kontakt" style={{ textDecoration: "none" }}>
-                  <Nav.Link
-                    as={"span"}
-                    style={
-                      props.currentPage === "contact"
-                        ? { borderBottom: "2px solid rgb(255 197 88)" }
-                        : {}
-                    }
-                    active={props.currentPage === "contact"}
-                  >
-                    Kontakt
-                  </Nav.Link>
-                </Link>
-              </Nav>
-              <Nav>
-                <Link to="/kestazeni" style={{ textDecoration: "none" }}>
-                  <Nav.Link
-                    as={"span"}
-                    style={
-                      props.currentPage === "download"
-                        ? { borderBottom: "2px solid rgb(255 197 88)" }
-                        : {}
-                    }
-                    active={props.currentPage === "download"}
-                  >
-                    Ke Stažení
-                  </Nav.Link>
-                </Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </ThemeProvider>
+          />
+        </div>
+      </Hidden>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Navbar.Brand>
+              <Hidden lg xl xxl xxxl>
+                <img
+                  src="\images\logo.png"
+                  alt="Logo"
+                  style={{
+                    backgroundColor: "#7c7c7c",
+                    padding: "2px",
+                    maxHeight: "32px",
+                    display: { md: "inherit", xs: "none" },
+                  }}
+                />
+              </Hidden>
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto" >
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Nav.Link
+                  as={"span"}
+
+                  style={
+                    props.currentPage === "main"
+                      ? { display: "flex", alignItems: "center", marginRight: "20px", borderBottom: "2px solid rgb(255 197 88)" }
+                      : { display: "flex", alignItems: "center", marginRight: "20px" }
+
+                  }
+                  active={props.currentPage === "main"}
+                >
+                  <House style={{ marginRight: "5px" }} />
+                  Úvod
+                </Nav.Link>
+              </Link>
+              <Link to="/technologie" style={{ textDecoration: "none" }}>
+                <Nav.Link
+                  as={"span"}
+                  style={
+                    props.currentPage === "technology"
+                      ? { display: "flex", alignItems: "center", marginRight: "20px", borderBottom: "2px solid rgb(255 197 88)" }
+                      : { display: "flex", alignItems: "center", marginRight: "20px" }
+                  }
+                  active={props.currentPage === "technology"}
+                >
+                  <Gear style={{ marginRight: "5px" }} />
+                  Technologie
+                </Nav.Link>
+              </Link>
+              <Link to="/fotografie" style={{ textDecoration: "none" }}>
+                <Nav.Link
+                  as={"span"}
+                  style={
+                    props.currentPage === "fotography"
+                      ? { display: "flex", alignItems: "center", marginRight: "20px", borderBottom: "2px solid rgb(255 197 88)" }
+                      : { display: "flex", alignItems: "center", marginRight: "20px" }
+                  }
+                  active={props.currentPage === "fotography"}
+                >
+                  <Image style={{ marginRight: "5px" }} />
+                  Fotografie
+                </Nav.Link>
+              </Link>
+              <Link to="/reference" style={{ textDecoration: "none" }}>
+                <Nav.Link
+                  as={"span"}
+                  style={
+                    props.currentPage === "reference"
+                      ? { display: "flex", alignItems: "center", marginRight: "20px", borderBottom: "2px solid rgb(255 197 88)" }
+                      : { display: "flex", alignItems: "center", marginRight: "20px" }
+                  }
+                  active={props.currentPage === "reference"}
+                >
+                  <Briefcase style={{ marginRight: "5px" }} />
+                  Referece
+                </Nav.Link>
+              </Link>
+              <Link to="/kontakt" style={{ textDecoration: "none" }}>
+                <Nav.Link
+                  as={"span"}
+                  style={
+                    props.currentPage === "contact"
+                      ? { display: "flex", alignItems: "center", marginRight: "20px", borderBottom: "2px solid rgb(255 197 88)" }
+                      : { display: "flex", alignItems: "center", marginRight: "20px" }
+                  }
+                  active={props.currentPage === "contact"}
+                >
+                  <Person style={{ marginRight: "5px" }} />
+                  Kontak
+                </Nav.Link>
+              </Link>
+            </Nav>
+            <Nav>
+              <Link to="/kestazeni" style={{ textDecoration: "none" }}>
+                <Nav.Link
+                  as={"span"}
+                  style={
+                    props.currentPage === "download"
+                      ? { display: "flex", alignItems: "center", borderBottom: "2px solid rgb(255 197 88)" }
+                      : { display: "flex", alignItems: "center" }
+                  }
+                  active={props.currentPage === "download"}
+                >
+                  <Download style={{ marginRight: "5px" }} />
+                  Ke Stažení
+                </Nav.Link>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar >
     </>
   );
 }

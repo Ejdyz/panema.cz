7a4050bc2f1ab89d30035b1f8263a9e24b8b55c4
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-18 22:44:30 +0200

    feat: Created fotocarousel component (need to set images)

---
+++ panema/src/Components/FotoCarousel.js
@@ -0,0 +1,46 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+
+export function FotoCarousel() {
+  return (
+    <Carousel variant="dark" >
+      <Carousel.Item interval={99999999999} >
+        <img
+          className="d-block w-100"
+          src="/images/trhani2.webp"
+          alt="First slide"
+          style={{ borderRadius: "20px" }}
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item >
+      <Carousel.Item interval={99999999999}>
+        <img
+          className="d-block w-100"
+          src="/images/vrtacka.webp"
+          alt="First slide"
+          style={{ borderRadius: "20px" }}
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={99999999999}>
+        <img
+          className="d-block w-100"
+          src="/images/vrtacka2.webp"
+          alt="First slide"
+          style={{ borderRadius: "20px" }}
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+    </Carousel >
+  );
+}

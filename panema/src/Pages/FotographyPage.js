a2b1c24629d72d9b9a3176bf1b50002186aef55a
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-19 11:05:41 +0200

    fix: responzive fotogalery

--- panema/src/Pages/FotographyPage.js
+++ panema/src/Pages/FotographyPage.js
@@ -15,13 +15,15 @@ export function FotographyPage() {
                 <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Fotogalerie</h1>
                 <hr style={{ width: "200px", color: "#e7b242", opacity: 1 }}></hr>
               </div>
-              <FotoCarousel />
+              <div style={{ width: "100%", height: "auto" }}>
+                <FotoCarousel />
+              </div>
               <br></br>
             </div>
           </Container>
         </Hidden>
         <Hidden sm md >
-          <Container style={{ backgroundColor: "#191c1e", height: "100vh" }}> {/* Content container */}
+          <Container style={{ backgroundColor: "#191c1e", height: "100%" }}> {/* Content container */}
             <div style={{ paddingTop: "3vh", height: "100%" }}>
               <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", position: "relative", flexWrap: "nowrap" }}>
                 <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Fotogalerie</h1>

745d6608b18a451953724d3600c7af5287a61147
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-19 09:17:44 +0200

    feat: mobile responzibility fotogalery page

--- panema/src/Pages/FotographyPage.js
+++ panema/src/Pages/FotographyPage.js
@@ -1,3 +1,4 @@
+import { Hidden } from 'react-grid-system'
 import { Footer } from '../Components/Footer'
 import { FotoCarousel } from '../Components/FotoCarousel'
 import NavBar from '../Components/NavBar'
@@ -6,17 +7,31 @@ export function FotographyPage() {
   return (
     <>
       <NavBar currentPage="fotography" />
-      <div style={{ backgroundColor: "#101214", height: "100%" }}> {/* Background */}
-        <Container style={{ backgroundColor: "#191c1e", height: "100%" }}> {/* Content container */}
-          <div style={{ paddingTop: "3vh", height: "100%" }}>
-            <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", position: "relative", flexWrap: "nowrap" }}>
-              <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Fotogalerie</h1>
-              <hr style={{ width: "200px", color: "#e7b242", opacity: 1 }}></hr>
+      <div style={{ backgroundColor: "#101214" }}> {/* Background */}
+        <Hidden lg xl xxl xxxl xs xxs>
+          <Container style={{ backgroundColor: "#191c1e", height: "100%" }}> {/* Content container */}
+            <div style={{ paddingTop: "3vh", height: "100%" }}>
+              <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", position: "relative", flexWrap: "nowrap" }}>
+                <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Fotogalerie</h1>
+                <hr style={{ width: "200px", color: "#e7b242", opacity: 1 }}></hr>
+              </div>
+              <FotoCarousel />
+              <br></br>
             </div>
-            <FotoCarousel />
-            <br></br>
-          </div>
-        </Container>
+          </Container>
+        </Hidden>
+        <Hidden sm md >
+          <Container style={{ backgroundColor: "#191c1e", height: "100vh" }}> {/* Content container */}
+            <div style={{ paddingTop: "3vh", height: "100%" }}>
+              <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", position: "relative", flexWrap: "nowrap" }}>
+                <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Fotogalerie</h1>
+                <hr style={{ width: "200px", color: "#e7b242", opacity: 1 }}></hr>
+              </div>
+              <FotoCarousel />
+              <br></br>
+            </div>
+          </Container>
+        </Hidden>
       </div>
       <Footer />
     </>

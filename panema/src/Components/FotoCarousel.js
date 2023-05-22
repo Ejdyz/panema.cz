1dd7a6cb45e1cbed156c2be236039b553ff06f78
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-22 12:14:45 +0200

    refactor: reformated code in fotoCarousel

--- panema/src/Components/FotoCarousel.js
+++ panema/src/Components/FotoCarousel.js
@@ -1,227 +1,247 @@
-import Carousel from 'react-bootstrap/Carousel';
-
+import Carousel from "react-bootstrap/Carousel";
 
 export function FotoCarousel() {
   return (
-    <Carousel variant="light" >
-      <Carousel.Item interval={99999999999}  >
+    <Carousel variant="light">
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/IMG_20230517_122204.jpg"
           alt="Naložený kamion"
         />
-      </Carousel.Item >
+      </Carousel.Item>
 
-      <Carousel.Item interval={99999999999}  >
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/IMG_20230517_122259.jpg"
           alt="Naložený kamion"
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/DJI_0286.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/DJI_0292.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/DJI_0296.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/DJI_0298.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20170313_102924.jpg"
           alt="Vrtání podel Labe"
         />
-      </Carousel.Item >
+      </Carousel.Item>
 
-      <Carousel.Item interval={99999999999}  >
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20170316_095950.jpg"
           alt="Vrtání podel Labe"
         />
-      </Carousel.Item >
+      </Carousel.Item>
 
-
-      <Carousel.Item interval={99999999999}  >
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20160819_092444.jpg"
           alt=""
         />
-      </Carousel.Item >
+      </Carousel.Item>
 
-      <Carousel.Item interval={99999999999}  >
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20160829_152220.jpg"
           alt=""
         />
-      </Carousel.Item >
+      </Carousel.Item>
 
-
-      <Carousel.Item interval={99999999999}  >
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20160829_152156.jpg"
-          alt="Hluboká jáma" />
-      </Carousel.Item >
+          alt="Hluboká jáma"
+        />
+      </Carousel.Item>
 
-      <Carousel.Item interval={99999999999}  >
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20190820_101834.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20190820_104305.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20190821_094504.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20190821_094536.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20190912_151333.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20190918_091959.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20191031_104448.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20200615_140453.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20200619_160752.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20201022_101703.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20211026_130259.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20220613_145141.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20220725_092817.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20220725_092835.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20220725_092840.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20220725_094146.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20230511_085356.jpg"
           alt=""
         />
-      </Carousel.Item >
+      </Carousel.Item>
 
-      <Carousel.Item interval={99999999999}  >
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/IMG_20230517_084608.jpg"
           alt=""
         />
-      </Carousel.Item >
-      <Carousel.Item interval={99999999999}  >
+      </Carousel.Item>
+
+      <Carousel.Item interval={99999999999}>
         <img
           className="d-block w-100"
           src="/fotogalery/20221102_123722.jpg"
           alt=""
         />
-      </Carousel.Item >
-    </Carousel >
+      </Carousel.Item>
+    </Carousel>
   );
 }

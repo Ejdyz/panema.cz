5d6bfbbab173e3fff421d5ebd62841154e925eb7
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-22 16:28:28 +0200

    feat: responsive reference page

--- panema/src/Pages/ReferencesPage.js
+++ panema/src/Pages/ReferencesPage.js
@@ -1,7 +1,9 @@
 import NavBar from '../Components/NavBar'
 import Container from 'react-bootstrap/esm/Container'
 import ReferenceComponent from '../Components/ReferenceComponent'
+import MobileReferenceComponent from '../Components/MobileReferenceComponent'
 import { Footer } from '../Components/Footer'
+import { Hidden } from 'react-grid-system'
 
 export function ReferencesPage() {
   return (
@@ -15,16 +17,34 @@ export function ReferencesPage() {
               <br />
             </div>
 
-            <ReferenceComponent name="SčVK a.s. Ústí n/L" logo='/logos/logo_scvk.svg' hr={true} />
-            <ReferenceComponent name="Eurovia a.s." logo='/logos/logo_eurovia.svg' hr={true} />
-            <ReferenceComponent name="Vodovody a kanalizace Náchod a.s." logo='/logos/logo_vakNachod.png' hr={true} />
-            <ReferenceComponent name="Vodárny a kanalizace Karlovy Vary, a.s." logo='/logos/logo_vodakva.png' hr={true} />
-            <ReferenceComponent name="Vodovody a kanalizace Týnec s.r.o." logo='/logos/logo_vakTynec.png' hr={true} />
-            <ReferenceComponent name="Jukr spol. s.r.o." logo='/logos/logo_jukr.png' hr={true} />
-            <ReferenceComponent name="ELMOS LIBEREC s.r.o." logo='/logos/logo_elmos.png' hr={true} />
-            <ReferenceComponent name="ERKA ŽATEC s.r.o." logo='/logos/logo_erka.png' hr={true} />
-            <ReferenceComponent name="SčVK a.s. Ústí n/L" logo='/logos/logo_laros.jpg' />
-
+            <Hidden md sm xs xxs>
+              <ReferenceComponent name="SčVK a.s." logo='/logos/logo_scvk.svg' hr={true} />
+              <ReferenceComponent name="SVS a.s." logo='/logos/logo_svs.png' hr={true} />
+              <ReferenceComponent name="Severočeská servisní a.s." logo='/logos/logo_severoceskaServisni.png' hr={true} />
+              <ReferenceComponent name="Eurovia a.s." logo='/logos/logo_eurovia.svg' hr={true} />
+              <ReferenceComponent name="Metrostav a.s." logo='/logos/logo_metrostav.svg' hr={true} />
+              <ReferenceComponent name="Vodovody a kanalizace Náchod a.s." logo='/logos/logo_vakNachod.png' hr={true} />
+              <ReferenceComponent name="Vodárny a kanalizace Karlovy Vary, a.s." logo='/logos/logo_vodakva.png' hr={true} />
+              <ReferenceComponent name="Vodovody a kanalizace Týnec s.r.o." logo='/logos/logo_vakTynec.png' hr={true} />
+              <ReferenceComponent name="EMSL LIBEREC s.r.o." logo='/logos/logo_emsl.png' hr={true} />
+              <ReferenceComponent name="ERKA ŽATEC s.r.o." logo='/logos/logo_erka.png' hr={true} />
+              <ReferenceComponent name="Laros s.r.o." logo='/logos/logo_laros.jpg' hr={true} />
+              <ReferenceComponent name="Kollert Elektro s.r.o." logo='/logos/logo_kollert.webp' hr={false} />
+            </Hidden>
+            <Hidden lg xl xxl xxxl>
+              <MobileReferenceComponent name="SčVK a.s." logo='/logos/logo_scvk.svg' hr={true} />
+              <MobileReferenceComponent name="SVS a.s." logo='/logos/logo_svs.png' hr={true} />
+              <MobileReferenceComponent name="Severočeská servisní a.s." logo='/logos/logo_severoceskaServisni.png' hr={true} />
+              <MobileReferenceComponent name="Eurovia a.s." logo='/logos/logo_eurovia.svg' hr={true} />
+              <MobileReferenceComponent name="Metrostav a.s." logo='/logos/logo_metrostav.svg' hr={true} />
+              <MobileReferenceComponent name="Vodovody a kanalizace Náchod a.s." logo='/logos/logo_vakNachod.png' hr={true} />
+              <MobileReferenceComponent name="Vodárny a kanalizace Karlovy Vary, a.s." logo='/logos/logo_vodakva.png' hr={true} />
+              <MobileReferenceComponent name="Vodovody a kanalizace Týnec s.r.o." logo='/logos/logo_vakTynec.png' hr={true} />
+              <MobileReferenceComponent name="EMSL LIBEREC s.r.o." logo='/logos/logo_emsl.png' hr={true} />
+              <MobileReferenceComponent name="ERKA ŽATEC s.r.o." logo='/logos/logo_erka.png' hr={true} />
+              <MobileReferenceComponent name="Laros s.r.o." logo='/logos/logo_laros.jpg' hr={true} />
+              <MobileReferenceComponent name="Kollert Elektro s.r.o." logo='/logos/logo_kollert.webp' hr={false} />
+            </Hidden>
 
           </div>
         </Container >

9b2e2b77a50368e71d97eb9f82a1fbca5ec7b349
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-22 18:59:58 +0200

    feat: corrected text on main page + bigger gradient from image to text

--- panema/src/Components/MainPageTextCard.js
+++ panema/src/Components/MainPageTextCard.js
@@ -8,8 +8,7 @@ export function MainPageTextCard() {
         </div>
         <br />
         <h4>Společnost <h4 style={{ color: "#e0a535", display: "inline" }}>PANEMA </h4> s.r.o. byla založena v roce 2004 a zabývá se bezvýkopou výstavbou a rekonstrukcí inženýrských sítí.</h4>
-        <br />
-        <h4><h4 style={{ color: "#e0a535", display: "inline" }}>Rádi</h4> vám poradíme jakou technologii při stavebních pracech zvolit. </h4>
+
         <br />
         <u><h4>Naším cílem je  <u style={{ color: '#e0a535' }}><h4 style={{ color: "#e0a535", display: "inline" }}>spokojený</h4> </u>zákazník!</h4></u>
       </div>

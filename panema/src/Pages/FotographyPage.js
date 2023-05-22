7bdf2225f78ae949221f6cc79f059b0016468390
Ejdyz <adamhonzikcz@seznam.cz>
2023-05-22 14:55:37 +0200

    style: changed hr under text to border bottom

--- panema/src/Pages/FotographyPage.js
+++ panema/src/Pages/FotographyPage.js
@@ -12,8 +12,8 @@ export function FotographyPage() {
           <Container style={{ backgroundColor: "#191c1e", height: "100%" }}> {/* Content container */}
             <div style={{ paddingTop: "3vh", minHeight: "100vh" }}>
               <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", position: "relative", flexWrap: "nowrap" }}>
-                <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Fotogalerie</h1>
-                <hr style={{ width: "200px", color: "#e7b242", opacity: 1 }}></hr>
+                <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem", borderBottom: "#e7b242 2px solid" }}>Fotogalerie</h1>
+                <br />
               </div>
               <div style={{ width: "100%", height: "auto" }}>
                 <FotoCarousel />
@@ -26,8 +26,8 @@ export function FotographyPage() {
           <Container style={{ backgroundColor: "#191c1e", height: "100%" }}> {/* Content container */}
             <div style={{ paddingTop: "3vh", minHeight: "100vh" }}>
               <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", position: "relative", flexWrap: "nowrap" }}>
-                <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Fotogalerie</h1>
-                <hr style={{ width: "200px", color: "#e7b242", opacity: 1 }}></hr>
+                <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem", borderBottom: "#e7b242 2px solid" }}>Fotogalerie</h1>
+                <br />
               </div>
               <FotoCarousel />
               <br></br>
--- panema/src/Pages/ReferencesPage.js
+++ panema/src/Pages/ReferencesPage.js
@@ -1,19 +1,35 @@
 import NavBar from '../Components/NavBar'
 import Container from 'react-bootstrap/esm/Container'
+import ReferenceComponent from '../Components/ReferenceComponent'
+import { Footer } from '../Components/Footer'
+
 export function ReferencesPage() {
   return (
     <>
       <NavBar currentPage="reference" />
-      <div style={{ backgroundColor: "#101214", height: "100vh" }}> {/* Background */}
+      <div style={{ backgroundColor: "#101214", height: "100%" }}> {/* Background */}
         <Container style={{ backgroundColor: "#191c1e", minHeight: "100vh" }}> {/* Content container */}
           <div style={{ paddingTop: "3vh", height: "100%" }}>
             <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", position: "relative", flexWrap: "nowrap" }}>
-              <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem" }}>Reference</h1>
-              <hr style={{ width: "200px", color: "#e7b242", opacity: 1 }}></hr>
+              <h1 style={{ textAlign: "center", color: "white", fontSize: "3rem", borderBottom: "#e7b242 2px solid" }}>Reference</h1>
+              <br />
             </div>
+
+            <ReferenceComponent name="SčVK a.s. Ústí n/L" logo='/logos/logo_scvk.svg' hr={true} />
+            <ReferenceComponent name="Eurovia a.s." logo='/logos/logo_eurovia.svg' hr={true} />
+            <ReferenceComponent name="Vodovody a kanalizace Náchod a.s." logo='/logos/logo_vakNachod.png' hr={true} />
+            <ReferenceComponent name="Vodárny a kanalizace Karlovy Vary, a.s." logo='/logos/logo_vodakva.png' hr={true} />
+            <ReferenceComponent name="Vodovody a kanalizace Týnec s.r.o." logo='/logos/logo_vakTynec.png' hr={true} />
+            <ReferenceComponent name="Jukr spol. s.r.o." logo='/logos/logo_jukr.png' hr={true} />
+            <ReferenceComponent name="ELMOS LIBEREC s.r.o." logo='/logos/logo_elmos.png' hr={true} />
+            <ReferenceComponent name="ERKA ŽATEC s.r.o." logo='/logos/logo_erka.png' hr={true} />
+            <ReferenceComponent name="SčVK a.s. Ústí n/L" logo='/logos/logo_laros.jpg' />
+
+
           </div>
-        </Container>
-      </div>
+        </Container >
+        <Footer />
+      </div >
     </>
   )
 }
